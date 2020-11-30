ea70f85b7d62422f45ec26dd16d44b4f08f6c4f2
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2020-11-30 17:16:18 +0900

    Add Top to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/molecules/Header'
+import Top from './components/molecules/Top'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Top />
       </Router>
     </div>
   );
