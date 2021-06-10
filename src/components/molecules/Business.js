5495ad59562accfe65686695eec960f456661f83
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2021-06-10 13:31:47 +0900

    Add page title to Business

---
+++ src/components/molecules/Business.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Business.css';
+
+class Business extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2 className="heading-business" data-en="Business">ビジネス</h2>
+            </div>
+        );
+    }
+}
+
+export default Business
