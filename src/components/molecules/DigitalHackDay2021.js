e3c741a57eb03359808ec8e6f7dd48baf71f4851
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2021-09-25 19:46:33 +0900

    Add title to DigitalHackDay2021 page

---
+++ src/components/molecules/DigitalHackDay2021.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DigitalHackDay2021.css';
+
+class DigitalHackDay2021 extends React.Component {
+    render() {
+        return (
+            <div className="project-detail-main">
+                <h2 className="heading-project-detail-title" data-en="Kenko">けんこちゃん</h2>
+            </div>
+        );
+    }
+}
+
+export default DigitalHackDay2021
