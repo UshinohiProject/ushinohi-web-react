b8e0f212f17c5284f1e8d4789d1efc7b76b3309a
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2021-09-25 19:58:00 +0900

    Add an image of Kenko-chan

--- src/components/molecules/DigitalHackDay2021.js
+++ src/components/molecules/DigitalHackDay2021.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './DigitalHackDay2021.css';
 
+import Kenko from './img/projects/kenko_big.jpg'
+
 class DigitalHackDay2021 extends React.Component {
     render() {
         return (
             <div className="project-detail-main">
                 <h2 className="heading-project-detail-title" data-en="Kenko">けんこちゃん</h2>
+                <div class="project-detail">
+                	<img class="project-detail-top-pic" src={Kenko} alt="Kenko-chan."></img>
+                </div>
             </div>
         );
     }
