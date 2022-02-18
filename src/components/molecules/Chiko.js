577c072d552cf2e86047c318e2d3c2f9780eaebc
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2022-02-19 06:39:59 +0900

    Create Chiko

---
+++ src/components/molecules/Chiko.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Chiko.css';
+
+class Chiko extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>遅刻Generator</h2>
+            </div>
+        );
+    }
+}
+
+export default Chiko
