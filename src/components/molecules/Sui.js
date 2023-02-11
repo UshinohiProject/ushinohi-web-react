e98fd5eaa27e349003aec33f792d8e9ec3e60d28
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2023-02-11 19:59:44 +0900

    Add Sui

---
+++ src/components/molecules/Sui.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import './Sui.css';
+
+class Sui extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    handleClick() {
+        this.props.history.push({
+            pathname: "/Suied",
+            state: { text: this.state.text }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <p>何で喧嘩しちゃったの？</p>
+                <br />
+                <input type='text' value={this.state.text} onChange={this.handleChange}></input>
+                <p>{this.state.text}</p>
+                <button onClick={this.handleClick}>水に流す</button>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Sui)
